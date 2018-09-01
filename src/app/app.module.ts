5c645a39fff91a1831ab0da49a20b1d187538ea5
norvey <norveyg@gmail.com>
2018-09-01 15:36:02 -0500

    Ionic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,12 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { TransactionsPage } from '../pages/transactions/transactions';
+import {AddingPage} from '../pages/adding/adding';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +20,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    TransactionsPage,
+    AddingPage
   ],
   imports: [
     BrowserModule,
@@ -29,7 +34,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    TransactionsPage,
+    AddingPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/adding/adding.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Transaction} from '../../database';
+
+/**
+ * Generated class for the AddingPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-adding',
+  templateUrl: 'adding.html',
+})
+export class AddingPage {
+  model : Transaction= new Transaction(null,"");
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    
+   
+  }
+
+}
